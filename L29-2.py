a2aa8a985324bba4d9208287c6e16f13f8786b6e
Yuliia <121313266+Yuliia-K27@users.noreply.github.com>
2023-03-30 11:28:00 +0300

    Update L29-2.py

--- L29-2.py
+++ L29-2.py
@@ -1,53 +1,78 @@
-import hashlib  # імпорт модуля для хешування паролів
+# імпорт модуля для хешування паролів
+import hashlib  
 
-# словник для зберігання пар "логін-пароль"
-user_data = {}
-
-
-# Функція реєстрації користувача
-def register():
-    print("***** РЕЄСТРАЦІЯ КОРИСТУВАЧА *****")
-    login = input("Введіть логін: ")
-
-    # перевірка чи вже є користувач з таким логіном
-    if login in user_data:
-        print("Користувач з таким логіном вже існує!")
-        return
+class HashTable:
+    def __init__(self, size=100):
+        self.size = size
+        # створюємо порожні списки для кожного індексу таблиці
+        self.table = [[] for _ in range(size)] 
+        
+    def _hash(self, key):
+        return int(hashlib.sha256(key.encode('utf-8')).hexdigest(), 16) % self.size
     
-    # перевірка колізій
-    if hashed_password in user_data.values():
-        print("Цей пароль вже використовується!")
-        return
+    # додавання елемента до хеш-таблиці
+    def add(self, key, value):
+        hashed_key = self._hash(key)
+        for pair in self.table[hashed_key]:
+            # перевірка чи існує вже елемент з таким ключем
+            if pair[0] == key:
+                pair[1] = value
+                return
+        # якщо елемент з таким ключем не знайдено, додаємо новий елемент
+        self.table[hashed_key].append([key, value])
 
-    # запит пароля і його хешування за допомогою SHA-256
-    password = input("Введіть пароль: ")
-    hashed_password = hashlib.sha256(password.encode()).hexdigest()
+    # отримання елемента з хеш-таблиці по ключу
+    def get(self, key):
+        hashed_key = self._hash(key)
+        for pair in self.table[hashed_key]:
+            if pair[0] == key:
+                return pair[1]
+        # якщо елемент з таким ключем не знайдено, викидаємо виняток
+        raise KeyError(key)
 
-    # збереження пари "логін-хеш пароля" в словнику
-    user_data[login] = hashed_password
-    print("Користувач зареєстрований успішно!")
+    # видалення елемента з хеш-таблиці по ключу
+    def remove(self, key):
+        hashed_key = self._hash(key)
+        for i, pair in enumerate(self.table[hashed_key]):
+            if pair[0] == key:
+                del self.table[hashed_key][i]
+                return
+        # якщо елемент з таким ключем не знайдено, викидаємо виняток
+        raise KeyError(key)
 
+class User:
+    def __init__(self, login, password):
+        self.login = login
+        self.password_hash = hashlib.sha256(password.encode('utf-8')).hexdigest()
 
-# Функція авторизації користувача
-def login():
-    print("***** АВТОРИЗАЦІЯ КОРИСТУВАЧА *****")
-    login = input("Введіть логін: ")
+class AuthSystem:
+    def __init__(self):
+        self.users = HashTable()
 
-    # перевірка чи існує користувач з таким логіном
-    if login not in user_data:
-        print("Користувач з таким логіном не зареєстрований!")
-        return
+    # реєстрація користувача з унікальним логіном та хешованим паролем
+    def register_user(self):
+        login = input("Enter login: ")
+        password = input("Enter password: ")
+        # первірка колізій
+        if self.users.get(login):
+            # перевірка на унікальність логіну
+            print("User with such login already exists!")
+            return
+        user = User(login, password)
+        self.users.add(user.login, user)
+        print("User registered successfully!")
 
-    # запит пароля і перевірка його правильності
-    password = input("Введіть пароль: ")
-    hashed_password = hashlib.sha256(password.encode()).hexdigest()
-
-    if hashed_password == user_data[login]:
-        print("Вхід виконано успішно!")
-    else:
-        print("Неправильний пароль!")
-
-
+    def login(self):
+        # авторизація користувача з логіном та паролем
+        login = input("Enter login: ")
+        password = input("Enter password: ")
+        user = self.users.get(login)
+        if user.password_hash == hashlib.sha256(password.encode('utf-8')).hexdigest():
+            print("Authorized!")
+        else:
+            print("Wrong login or password.")
+        
 # виклик функцій реєстрації та авторизації користувача
-register()
-login()
+auth_system = AuthSystem()
+auth_system.register_user()
+auth_system.login()
