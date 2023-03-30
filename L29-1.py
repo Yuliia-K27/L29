c9a60eebcf18e5d659e5abae3fec2929cd55b8fb
Yuliia <121313266+Yuliia-K27@users.noreply.github.com>
2023-03-30 10:41:45 +0300

    Виправлення 1 (після перевірки)
    *Ви використовуєте доволі велику кількість індексів, а наприкінці ці індекси ще й перестають бути інформативними - це сильно ускладнює читабельність коду, намагайтесь уникати індексів i, j, k, h і тд:

--- L29-1.py
+++ L29-1.py
@@ -1,62 +1,51 @@
 class HashTable:
-    def __init__(self, size):
-        # конструктор класу, створює хеш-таблицю заданої довжини
+    def __init__(self, size):               # конструктор класу, створює хеш-таблицю заданої довжини
         self.size = size
         # ініціалізуємо список для бакетів, кожен бакет - порожній список
         self.table = [[] for _ in range(size)]
 
-    def hash_function(self, key):
-        # метод для визначення індексу бакета за ключем
+    def hash_function(self, key):             # метод для визначення індексу бакета за ключем
         return key % self.size
 
-    def add_element(self, key, value):
-        # метод додає новий елемент до хеш-таблиці за допомогою ключа і значення
-        index = self.hash_function(key)
-        # перевірка, чи елемент з таким ключем вже існує в бакеті
-        for i in range(len(self.table[index])):
-            if self.table[index][i][0] == key:
-                # якщо так, то замінюємо його значення
-                self.table[index][i] = (key, value)
+    def add_element(self, key, value):        # метод додає новий елемент до хеш-таблиці за допомогою ключа і значення
+        index = self.hash_function(key)       # перевірка, чи елемент з таким ключем вже існує в бакеті
+        for bucket_item in self.table[index]:
+            if bucket_item[0] == key:          # якщо так, то замінюємо його значення
+                bucket_item = (key, value)
                 return
-        # якщо елемент з таким ключем не існує, додаємо його до бакету
-        self.table[index].append((key, value))
+        self.table[index].append((key, value))  # якщо елемент з таким ключем не існує, додаємо його до бакету
 
-    def delete_element(self, key):
-        # метод видаляє елемент з хеш-таблиці за допомогою ключа
+    def delete_element(self, key):               # метод видаляє елемент з хеш-таблиці за допомогою ключа
         index = self.hash_function(key)
-        for i in range(len(self.table[index])):
-            if self.table[index][i][0] == key:
-                # якщо елемент знайдено, видаляємо його з бакету
-                del self.table[index][i]
+        for i, bucket_item in enumerate(self.table[index]):
+            if bucket_item[0] == key:
+                del self.table[index][i]            # якщо елемент знайдено, видаляємо його з бакету
                 return
 
-    def find_element(self, key):
-        # метод повертає значення елемента за його ключем або None, якщо елемент не знайдено
+    def find_element(self, key):                    # метод повертає значення елемента за його ключем або None, якщо елемент не знайдено
         index = self.hash_function(key)
-        for i in range(len(self.table[index])):
-            if self.table[index][i][0] == key:
-                # якщо елемент знайдено, повертаємо його значення
-                return self.table[index][i][1]
-        # якщо елемент не знайдено, повертаємо None
-        return None
+        for bucket_item in self.table[index]:
+            if bucket_item[0] == key:
+                return bucket_item[1]                # якщо елемент знайдено, повертаємо його значення
+        return None                                   # якщо елемент не знайдено, повертаємо None
 
     def print_table(self):
-        # метод друкує всі елементи хеш-таблиці
-        for i in range(self.size):
+        for i, bucket in enumerate(self.table):        # метод друкує всі елементи хеш-таблиці
             print(i, end=' ')
-            for item in self.table[i]:
+            for bucket_item in bucket:
                 print('-->', end=' ')
-                print(item, end=' ')
+                print(bucket_item, end=' ')
             print()
 
     def resolve_collision(self):
-        # метод вирішує колізії, тобто випадки, коли два елементи мають один і той самий індекс бакета
-        for i in range(self.size):
-            for j in range(len(self.table[i])):
-                key = self.table[i][j][0]
-                value = self.table[i][j][1]
+       for i, bucket in enumerate(self.table):           # метод вирішує колізії, тобто випадки, коли два елементи мають один і той самий індекс бакета
+            for bucket_item in bucket:
+                key = bucket_item[0]
+                value = bucket_item[1]
                 index = self.hash_function(key)
-                # якщо елемент не знаходиться в правильному бакеті, переносимо його
-                if index != i:
-                    self.table[i].pop(j)
-                    self.table[index].append((key, value))
+                if index != i:                                 # якщо елемент не знаходиться в правильному бакеті, переносимо його
+                    self.table[index].append((key, value))     # додаємо елемент до правильного бакету
+                    self.table[i].remove((key, value))         # видаляємо елемент зі старого бакету
+                    
+                    
+                    
